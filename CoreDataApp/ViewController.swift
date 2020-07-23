d933124c7a361e9894be03d94c1d1ea1d4709c24
alok <maddy.lucky4u@gmail.com>
2020-07-23 19:42:10 +0530

    Initial Commit

---
+++ CoreDataApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataApp
+//
+//  Created by Haider Khan on 7/23/20.
+//  Copyright © 2020 Alok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
